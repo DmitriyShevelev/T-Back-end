7eae2c548cb8a879a3e130a4c4f5cdb88089d9df
DmitriyShevelev <94847800+DmitriyShevelev@users.noreply.github.com>
2023-02-25 19:32:27 +0200

    Add files via upload

---
+++ 144_Sol-DSS-telran-items/dss-telran-items/src/main/java/dsstelran/DssTelranItemsApplication.java
@@ -0,0 +1,13 @@
+package dsstelran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DssTelranItemsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DssTelranItemsApplication.class, args);
+	}
+
+}
---
+++ 144_Sol-DSS-telran-items/dss-telran-items/src/test/java/dsstelran/DssTelranItemsApplicationTests.java
@@ -0,0 +1,13 @@
+package dsstelran;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DssTelranItemsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
