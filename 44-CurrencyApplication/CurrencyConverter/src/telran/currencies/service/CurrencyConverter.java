4e4f213aac214ef4520370f62595df8ab356a413
DmitriyShevelev <94847800+DmitriyShevelev@users.noreply.github.com>
2023-02-25 17:52:38 +0200

    Add files via upload

---
+++ 44-CurrencyApplication/CurrencyConverter/src/telran/currencies/service/CurrencyConverter.java
@@ -0,0 +1,8 @@
+package telran.currencies.service;
+import java.util.*;
+
+public interface CurrencyConverter {
+double convert(String currencyCodeCountryFrom, String currencyCodeCountryTo , int amount);
+Map<String, List<String>> strongestCurrencies(int amount);
+Map<String, List<String>> weakestCurrencies(int amount);
+}
